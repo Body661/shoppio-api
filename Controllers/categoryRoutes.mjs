f05a3cf2b15aada76a2f9890007403e495c7a2ca
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-10 00:25:19 +0100

    add pagination to categories

--- Controllers/categoryRoutes.mjs
+++ Controllers/categoryRoutes.mjs
@@ -2,6 +2,9 @@ import CategoryModel from "../models/categoryModel.mjs";
 import slugify from "slugify";
 import expressAsyncHandler from "express-async-handler";
 
+// @desc add a new category
+// @route POST /api/categories
+// @ access Private
 export const addCategory = expressAsyncHandler(async (req, res) => {
     const {name} = req.body
 
@@ -9,11 +12,14 @@ export const addCategory = expressAsyncHandler(async (req, res) => {
     res.status(201).json(category)
 })
 
+// @desc Get all categories
+// @route GET /api/categories
+// @access Public
 export const getCategories = expressAsyncHandler(async (req, res) => {
-    try {
-        const categories = await CategoryModel.find()
-        res.status(200).json(categories)
-    } catch (err) {
-        res.status(400).json(err)
-    }
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 2;
+    const skip = (page - 1) * limit;
+
+    const categories = await CategoryModel.find().skip(skip).limit(limit)
+    res.status(200).json({page, amount: categories.length, data: categories})
 })

caeb87cc8d4862fc41eaca439dd375bd7fadc86e
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-10 01:05:54 +0100

    Update and Create category

--- Controllers/categoryRoutes.mjs
+++ Controllers/categoryRoutes.mjs
@@ -6,10 +6,10 @@ import expressAsyncHandler from "express-async-handler";
 // @route POST /api/categories
 // @ access Private
 export const addCategory = expressAsyncHandler(async (req, res) => {
-    const {name} = req.body
+    const {name} = req.body;
 
     const category = await CategoryModel.create({name, slug: slugify(name)})
-    res.status(201).json(category)
+    res.status(201).json(category);
 })
 
 // @desc Get all categories
@@ -20,6 +20,35 @@ export const getCategories = expressAsyncHandler(async (req, res) => {
     const limit = req.query.limit * 1 || 2;
     const skip = (page - 1) * limit;
 
-    const categories = await CategoryModel.find().skip(skip).limit(limit)
-    res.status(200).json({page, amount: categories.length, data: categories})
+    const categories = await CategoryModel.find().skip(skip).limit(limit);
+    res.status(200).json({page, amount: categories.length, data: categories});
+})
+
+// @desc Get a specific category
+// @route GET /api/categories/:id
+// @access Public
+export const getCategory = expressAsyncHandler(async (req, res) => {
+    const category = await CategoryModel.findById(req.params.id);
+
+    if (!category) {
+        return res.status(404).json({message: 'Category not found'})
+    }
+
+    res.status(200).json(category);
+})
+
+// @desc Update a category
+// @route GET /api/categories/:id
+// @access Private
+export const updateCategory = expressAsyncHandler(async (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    const category = await CategoryModel.findByIdAndUpdate(id, {name, slug: slugify(req.body.name)}, {new: true})
+
+    if (!category) {
+        return res.status(404).json({message: 'Category not found'})
+    }
+
+    res.status(200).json(category);
 })
--- Routes/category.mjs
+++ Routes/category.mjs
@@ -1,10 +1,12 @@
 import express from "express";
-import {addCategory, getCategories} from "../Controllers/categoryRoutes.mjs";
+import {addCategory, getCategories, getCategory, updateCategory} from "../Controllers/categoryRoutes.mjs";
 
 const router = express.Router();
 
 router.route('/')
     .post(addCategory)
     .get(getCategories)
+
+router.route('/:id').get(getCategory).put(updateCategory)
 
 export default router

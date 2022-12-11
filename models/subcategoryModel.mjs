a589bd9f9627d6885594d506cdd9dcdab9c73937
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-11 21:43:55 +0100

    Update subcategoryModel.mjs

--- models/subcategoryModel.mjs
+++ models/subcategoryModel.mjs
@@ -2,10 +2,15 @@ import { Mongoose } from "mongoose";
 
 const SubcategoryScema = new Mongoose.Schema({
   name: {
-    type: "string",
+    type: String,
     required: true,
-    minLength: 3,
-    maxLength: 32,
+    minLength: [3, "Subcategory name must be at least 3 characters"],
+    maxLength: [32, "Subcategory name is too long"],
+    unique: true,
+  },
+  slug: {
+    type: String,
+    lowercase: true,
   },
 });
 

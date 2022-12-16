85b9e1fc46d3f5313bee9a29d86865784790d9e6
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-16 20:23:37 +0100

    Update subcategoryRoutes.mjs

--- Controllers/subcategoryRoutes.mjs
+++ Controllers/subcategoryRoutes.mjs
@@ -3,7 +3,7 @@ import expressAsyncHandler from "express-async-handler";
 import ApiError from "../utils/apiError.mjs";
 import SubcategoryModel from "../models/subcategoryModel.mjs";
 
-const addSubcategory = expressAsyncHandler(async (req, res, next) => {
+export const addSubcategory = expressAsyncHandler(async (req, res, next) => {
   const { name } = req.body;
 
   const subcategory = await SubcategoryModel.create({
@@ -12,3 +12,7 @@ const addSubcategory = expressAsyncHandler(async (req, res, next) => {
   });
   res.status(201).json(subcategory);
 });
+
+export const deleteSubcategory = expressAsyncHandler(
+  async (req, res, next) => {}
+);

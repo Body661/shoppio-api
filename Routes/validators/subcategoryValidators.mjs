09a3037487c13106558ceebfef640fcfe390976e
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-17 02:21:56 +0100

    Create subcategory validator layer

---
+++ Routes/validators/subcategoryValidators.mjs
@@ -0,0 +1,36 @@
+import { check } from "express-validator";
+import validatorMiddleware from "../../middlewares/validatorMiddleware.mjs";
+
+export const createSubcategoryValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("Subcategory name is required")
+    .isLength({ min: 2, max: 33 })
+    .withMessage("Category name must be between 2 and 32 characters"),
+
+  check("category")
+    .notEmpty()
+    .withMessage("Subcategory must be belong to a category")
+    .isMongoId()
+    .withMessage("Category ID is not valid"),
+  validatorMiddleware,
+];
+
+export const getSubcategoryValidator = [
+  check("id").isMongoId().withMessage("Subcategory ID is not valid"),
+  validatorMiddleware,
+];
+export const updateSubcategoryValidator = [
+  check("id").isMongoId().withMessage("Subcategory ID is not valid"),
+  check("name")
+    .notEmpty()
+    .withMessage("Subcategory name is required")
+    .isLength({ min: 2, max: 32 })
+    .withMessage("Subcategory name must be between 2 and 32 characters"),
+  validatorMiddleware,
+];
+
+export const deleteSubcategoryValidator = [
+  check("id").isMongoId().withMessage("Subcategory ID is not valid"),
+  validatorMiddleware,
+];

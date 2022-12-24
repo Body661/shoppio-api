c076c71c539b02393811346806bb7b0ce6b856fa
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-24 19:33:14 +0100

    Refactor error middleware

--- middlewares/errorMiddleware.mjs
+++ middlewares/errorMiddleware.mjs
@@ -1,3 +1,11 @@
+const devError = (err, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+
 const errorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -8,14 +16,4 @@ const errorMiddleware = (err, req, res, next) => {
 
   res.status(err.statusCode).json({ status: err.status, message: err.message });
 };
-
-const devError = (err, res) => {
-  return res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
 export default errorMiddleware;

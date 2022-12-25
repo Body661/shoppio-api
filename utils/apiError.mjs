9334c00966821d1fa470300d7d18e884c19f1f8b
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-25 12:38:32 +0100

    Refactor API Error

--- utils/apiError.mjs
+++ utils/apiError.mjs
@@ -1,10 +1,10 @@
 // @desc This class is responsible for creating error messages
 class ApiError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-        this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+  }
 }
 
-export default ApiError
+export default ApiError;

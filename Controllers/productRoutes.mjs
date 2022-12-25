fe5b00018678186884b79e832ef4bebcdb3c5a01
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2022-12-25 22:33:59 +0100

    API features
    Add API features and refactor products services to use it

--- Controllers/productRoutes.mjs
+++ Controllers/productRoutes.mjs
@@ -2,6 +2,7 @@ import expressAsyncHandler from "express-async-handler";
 import slugify from "slugify";
 import ProductModel from "../models/productModel.mjs";
 import ApiError from "../utils/apiError.mjs";
+import ApiFeatures from "../utils/apiFeatures.mjs";
 
 //@desc Create new product
 //@route POST /api/products
@@ -16,63 +17,25 @@ export const addProduct = expressAsyncHandler(async (req, res) => {
 // @route GET /api/products
 // @access Public
 export const getProducts = expressAsyncHandler(async (req, res) => {
-  //1) Pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 50;
-  const skip = (page - 1) * limit;
-
-  //2) Filtering
-  const filter = { ...req.query };
-  const excludesFileds = ["page", "sort", "limit", "fields"];
-  excludesFileds.forEach((field) => delete filter[field]);
-
-  // Apply filters using [gte | gt | lte | lt]
-  let queryString = JSON.stringify(filter);
-  queryString = queryString.replace(
-    /(gte|gt|lte|lt)\b/g,
-    (match) => `$${match}`
-  );
-
-  // Build query
-  let query = ProductModel.find(JSON.parse(queryString))
-    .skip(skip)
-    .limit(limit)
-    .populate([
-      { path: "category", select: "name -_id" },
-      { path: "subcategories", select: "name -_id" },
-      { path: "brand", select: "name -_id" },
-    ]);
-
-  //3) Sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  //4) Fields Limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").join(" ");
-    query = query.select(fields);
-  } else {
-    query = query.select("-__v");
-  }
-
-  //5) Search
-  if (req.query.keyword) {
-    const searchQuery = {};
-    searchQuery.$or = [
-      { title: { $regex: req.query.keyword, $options: "i" } },
-      { description: { $regex: req.query.keyword, $options: "i" } },
-    ];
-
-    query = query.find(searchQuery);
-  }
+  const docsCount = await ProductModel.countDocuments();
+  const apiFeatures = new ApiFeatures(ProductModel.find(), req.query)
+    .paginate(docsCount)
+    .filter()
+    .search()
+    .limit()
+    .sort();
+  // .populate([
+  //   { path: "category", select: "name -_id" },
+  //   { path: "subcategories", select: "name -_id" },
+  //   { path: "brand", select: "name -_id" },
+  // ]);
 
   // Execute query
-  const products = await query;
-  res.status(200).json({ page, amount: products.length, data: products });
+  const { query, paginationRes } = apiFeatures;
+  const products = await apiFeatures.query;
+  res
+    .status(200)
+    .json({ amount: products.length, paginationRes, data: products });
 });
 
 // @desc Get a list of all products
---
+++ utils/apiFeatures.mjs
@@ -0,0 +1,85 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const filter = { ...this.queryString };
+    const excludesFields = ["page", "sort", "limit", "fields"];
+    excludesFields.forEach((field) => delete filter[field]);
+
+    // Apply filters using [gte | gt | lte | lt]
+    let queryString = JSON.stringify(filter);
+    queryString = queryString.replace(
+      /(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    this.query = this.query.find(JSON.parse(queryString));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+
+    return this;
+  }
+
+  limit() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+
+    return this;
+  }
+
+  search() {
+    if (this.queryString.keyword) {
+      const searchQuery = {};
+      searchQuery.$or = [
+        { title: { $regex: this.queryString.keyword, $options: "i" } },
+        { description: { $regex: this.queryString.keyword, $options: "i" } },
+      ];
+
+      this.query = this.query.find(searchQuery);
+    }
+
+    return this;
+  }
+
+  paginate(docsCount) {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 50;
+    const skip = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const pagination = {};
+    pagination.currentPage = page;
+    pagination.limit = limit;
+    pagination.pages = Math.ceil(docsCount / limit);
+
+    if (endIndex < docsCount) {
+      pagination.next = page + 1;
+    }
+
+    if (skip > 0) {
+      pagination.prev = page - 1;
+    }
+
+    this.query = this.query.skip(skip).limit(limit);
+    this.paginationRes = pagination;
+
+    return this;
+  }
+}
+
+export default ApiFeatures;

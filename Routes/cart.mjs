83e8df49e311c635af98c58792f5f436aaa87f4f
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2023-01-13 18:09:29 +0100

    cart validators

--- Routes/cart.mjs
+++ Routes/cart.mjs
@@ -8,13 +8,26 @@ import {
   updateItemQuantity,
 } from "../Controllers/cartController.mjs";
 import { allowed, auth } from "../Controllers/authController.mjs";
+import {
+  addProductToCartValidator,
+  applyCouponValidator,
+  removeItemFromCartValidator,
+  updateItemQuantityValidator,
+} from "./validators/cartValidators.mjs";
 
 const router = express.Router();
 
 router.use(auth, allowed("user"));
 
-router.route("/").post(addProductToCart).get(getMyCart).delete(deleteMyCart);
-router.route("/:id").delete(removeItemFromCart).put(updateItemQuantity);
-router.post("/coupon", applyCoupon);
+router
+  .route("/")
+  .post(...addProductToCartValidator, addProductToCart)
+  .get(getMyCart)
+  .delete(deleteMyCart);
+router
+  .route("/:id")
+  .delete(...removeItemFromCartValidator, removeItemFromCart)
+  .put(...updateItemQuantityValidator, updateItemQuantity);
+router.post("/coupon", ...applyCouponValidator, applyCoupon);
 
 export default router;
---
+++ Routes/validators/cartValidators.mjs
@@ -0,0 +1,36 @@
+import { check } from "express-validator";
+import validatorMiddleware from "../../middlewares/validatorMiddleware.mjs";
+
+export const addProductToCartValidator = [
+  check("productId")
+    .notEmpty()
+    .withMessage("product Id is required")
+    .isMongoId()
+    .withMessage("Product id is not valid"),
+  check("quantity")
+    .optional()
+    .notEmpty()
+    .withMessage("Quantity cant not be an empty value")
+    .isNumeric()
+    .withMessage("Quantity must be a number"),
+  check("color")
+    .optional()
+    .notEmpty()
+    .withMessage("Color cant not be an empty value"),
+  validatorMiddleware,
+];
+
+export const removeItemFromCartValidator = [
+  check("id").isMongoId().withMessage("product ID is not valid"),
+  validatorMiddleware,
+];
+
+export const updateItemQuantityValidator = [
+  check("id").isMongoId().withMessage("product ID is not valid"),
+  validatorMiddleware,
+];
+
+export const applyCouponValidator = [
+  check("coupon").notEmpty().withMessage("Coupon code is required"),
+  validatorMiddleware,
+];

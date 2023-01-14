7776cb9bd45cb0453c1bc071a19407bef8675d39
Abdolrahman Saleh <abdelrahmanelhagrasy661@gmail.com>
2023-01-14 18:50:40 +0100

    set request size

--- server.mjs
+++ server.mjs
@@ -36,7 +36,7 @@ const __dirname = dirname(__filename);
 dbConnect();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "20kb" }));
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
